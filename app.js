cda3789fc9dea79918096ad8519bb306919335f7
Borovikov <borovikovvaleksey@gmail.com>
2021-07-04 07:23:58 +0300

    change structure

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const authRouter = require("./controllers/routes/authRoutes");
+const cookieParser = require('cookie-parser')
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('api/v1', authRouter);
 
